f31b93e28f0b921046486dc4fdb565dc57113b5f
krumeto97 <krumeto97@abv.bg>
2020-07-19 22:00:25 +0300

    added tests

---
+++ GreenRedGame.Tests/GreenRedGameTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenRedGame.Tests
+{
+    [TestFixture]
+    public class GreenRedGameTests
+    {
+        GridOperations gridOperations = new GridOperations();
+
+        [TestCase]
+        public void FirstInputShouldWork()
+        {
+            
+            int[,] grid =
+            {
+                {0,0,0 },
+                {1,1,1 },
+                {0,0,0 }
+            };
+            Assert.AreEqual(5, gridOperations.IterateGrid(grid, 10, 0, 1));
+        }
+
+        [TestCase]
+        public void SecondInputShouldWork()
+        {
+
+            int[,] grid =
+            {
+                {1,0,0,1 },
+                {1,1,1,1 },
+                {0,1,0,0 },
+                {1,0,1,0 },
+            };
+            Assert.AreEqual(14, gridOperations.IterateGrid(grid, 15, 2, 2));
+        }
+    }
+}
